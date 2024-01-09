d98d54f88abcaf36356891d4453483502840c827
João Vítor <140274816+VitorAlvs@users.noreply.github.com>
2024-01-08 22:23:56 -0300

    Aula 01
    Automatização do cadastro de produtos

---
+++ Jornada Python - Hashtag Treinamentos/auxiliar.py
@@ -0,0 +1,13 @@
+import pyautogui 
+import time
+import pandas
+
+produtos = r"C:\Users\JoãoVítorSantosBR-iT\Documents\GitHub\Estudos\Jornada Python - Hashtag Treinamentos\produtos.csv"
+
+#encontrar localização do ponteiro do muouse em x,y
+time.sleep(5)
+print(pyautogui.position())
+
+
+# tabela = pandas.read_csv(produtos)
+# print(tabela)

e29acc556d54843ba10875f167bbbc47ff3489e6
João Vítor <140274816+VitorAlvs@users.noreply.github.com>
2024-03-27 15:05:38 -0300

    Início c#

---
+++ Formação .NET Developer - Dio/Fundamentos .NET/Introdução ao NET.cs
@@ -0,0 +1,6 @@
+//01 - Introdução ao .NET
+    // .Net    -> Plataforma de desenvolvimento unificado;
+    // C#      -> Linguagem de programação usada no .NET
+//02 - Diferenças .NET Framework(Legado) e .NET
+    //.NET Framework - "Amarrado ao windows"
+    //.NET - Multiplataforma

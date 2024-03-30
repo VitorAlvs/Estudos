eb31d336ab44701b5b8d06cd63de00ba80381f3d
João Vítor <140274816+VitorAlvs@users.noreply.github.com>
2024-03-30 16:15:24 -0300

    c#

---
+++ Formação .NET Developer - Dio/Fundamentos .NET/03 - Sintaxe e Tipos de Dados em C#/01 - Sintaxe e Identação/Classe/Program.cs
@@ -0,0 +1,8 @@
+﻿using Classe.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Vítor";
+pessoa1.Idade = 19;
+pessoa1.Apresentar();
+
--- Formação .NET Developer - Dio/Fundamentos .NET/Introdução ao NET.cs
+++
@@ -1,6 +0,0 @@
-//01 - Introdução ao .NET
-    // .Net    -> Plataforma de desenvolvimento unificado;
-    // C#      -> Linguagem de programação usada no .NET
-//02 - Diferenças .NET Framework(Legado) e .NET
-    //.NET Framework - "Amarrado ao windows"
-    //.NET - Multiplataforma

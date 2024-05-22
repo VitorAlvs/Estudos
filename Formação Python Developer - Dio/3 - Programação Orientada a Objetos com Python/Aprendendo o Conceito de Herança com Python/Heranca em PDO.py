3410cb70b7fee66f99efd01beb45fcd28f9b0c6b
João Vítor <140274816+VitorAlvs@users.noreply.github.com>
2024-05-22 10:39:03 -0300

    herança py

--- Formação Python Developer - Dio/3 - Programação Orientada a Objetos com Python/Aprendendo o Conceito de Herança com Python/Heranca em PDO.py
+++ Formação Python Developer - Dio/3 - Programação Orientada a Objetos com Python/Aprendendo o Conceito de Herança com Python/Heranca em PDO.py
@@ -1,7 +1,15 @@
-#Herança é a capacidade d euma classe filha derivar ou herdar as características e comportamentos da classe pai
+#Herança é a capacidade de uma classe filha derivar ou herdar as características e comportamentos da classe pai
 
-# Class a:
-#     pass
+class A:
+    pass
 
-# Class B(a):
-#     pass
+class B(A):
+    pass
+
+# Herança simples: A classe B herda de A
+# Heraça múltipla: A classe C herda de A e B
+
+class C:
+    pass
+class D(A, C):
+    pass

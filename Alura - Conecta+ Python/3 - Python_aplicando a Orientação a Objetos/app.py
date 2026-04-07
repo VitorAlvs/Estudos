389b46f3b4d7f80333858910c411234046fa65a5
João Vítor <140274816+VitorAlvs@users.noreply.github.com>
2026-04-07 12:49:34 -0300

    Finalização do curso

--- Alura - Conecta+ Python/3 - Python_aplicando a Orientação a Objetos/app.py
+++ Alura - Conecta+ Python/3 - Python_aplicando a Orientação a Objetos/app.py
@@ -1,10 +1,11 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca   = Restaurante('praça', 'gourmet')
-restaurante_bubble  = Restaurante('bubble', 'Frutos do Mar')
-restaurante_leaf    = Restaurante('Leaf', 'vegana')
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Luisa', 7)
+restaurante_praca.receber_avaliacao('Paulo', 5)
 
-restaurante_bubble.alternar_estado()
+restaurante_praca.alternar_estado()
 
 def main():
     Restaurante.listar_restaurantes()
--- Alura - Conecta+ Python/3 - Python_aplicando a Orientação a Objetos/modelos/restaurante.py
+++ Alura - Conecta+ Python/3 - Python_aplicando a Orientação a Objetos/modelos/restaurante.py
@@ -16,9 +16,9 @@ class Restaurante:
 
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do Restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | {'Estado'.ljust(20)}')
+        print(f'{'Nome do Restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | {'Avaliação'.ljust(20)} | {'Estado'.ljust(20)}')
         for each in cls.restaurantes:
-            print (f'{each._nome.ljust(20)} | {each._categoria.ljust(20)} | {each.estado}')
+            print (f'{each._nome.ljust(20)} | {each._categoria.ljust(20)} | {str(each.media_avaliacoes).ljust(20)} | {each.estado}')
 
     @property
     def estado(self):
@@ -28,4 +28,15 @@ class Restaurante:
         self._estado = not self._estado
 
     def receber_avaliacao(self, cliente, nota):
-        pass
+        if 0 < nota <= 5: 
+                avaliacao = Avaliacao(cliente, nota)
+                self._avaliacao.append(avaliacao)
+    
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round((soma_das_notas/quantidade_de_notas),1)
+        return media
